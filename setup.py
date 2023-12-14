1849113a8d8a85713814694557df2596a0b9b0fe
jjrom <jerome.gasperi@gmail.com>
2023-12-14 14:37:21 +0100

    Prepare for first 0.0.1 release

--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@ from setuptools import find_packages, setup
 setup(
     name='rapid',
     packages=find_packages(include=['rapid']),
-    version='1.0.0',
+    version='0.0.1',
     description='resto python API',
     author='Jérôme Gasperi',
     install_requires=[
         "requests"
-    ],
-    setup_requires=['pytest-runner'],
+    ]
 )
